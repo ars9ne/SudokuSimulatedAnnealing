34bae6fd18058a2588669f24bc71f347e628020a
ars9ne <arsenyrt@yandex.ru>
2023-11-13 07:51:25 +0300

    Added graph and ranomizer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-
+import matplotlib.pyplot as plt
 import numpy as np
 
 sudoku = np.array(
@@ -7,48 +7,79 @@ sudoku = np.array(
      [3, 0, 8, 6, 9, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 0, 2, 8], [0, 0, 6, 0, 0, 3, 2, 0, 0],
      [0, 0, 5, 1, 0, 0, 3, 9, 6], [8, 0, 2, 0, 4, 0, 0, 5, 0]])
 
-#Costfunction
+
+# Costfunction
 def costfunction(array):
-
-
     size = 9
-    #X SUMMATOR
+    # X SUMMATOR
     xlist = []
     j = 1
     for i in range(size):
-        unique, counts = np.unique(sudoku[i], return_counts=True)
-        #print(np.asarray((unique, counts)).T)
-        #print(len(unique)) # количество уникальных чисел в строке
-        #print(counts) # выводит количество каждого уникального символа в строке
+        unique, counts = np.unique(array[i], return_counts=True)
+        # print(np.asarray((unique, counts)).T)
+        # print(len(unique)) # количество уникальных чисел в строке
+        # print(counts) # выводит количество каждого уникального символа в строке
         for j in range(len(unique)):
             if counts[j] > 1:
                 xlist.append(counts[j])
 
-    print("ОСЬ Х: " + str(xlist))
+    #print("ОСЬ Х: " + str(xlist))
 
-    #Y SUMMATOR
+    # Y SUMMATOR
     ylist = []
-    rsudoku = np.rot90(sudoku) # переворачиваем по осям
-    #print(rsudoku)
+    rsudoku = np.rot90(array)  # переворачиваем по осям
+    # print(rsudoku)
     for i in range(size):
-        unique, counts = np.unique(rsudoku[i], return_counts=True)
-        #print(np.asarray((unique, counts)).T)
-        #print(len(unique)) # количество уникальных чисел в ряду
-        #print(counts) # выводит количество каждого уникального символа в ряду
+        unique, counts = np.unique(array[i], return_counts=True)
+        # print(np.asarray((unique, counts)).T)
+        # print(len(unique)) # количество уникальных чисел в ряду
+        # print(counts) # выводит количество каждого уникального символа в ряду
         for j in range(len(unique)):
             if counts[j] > 1:
                 ylist.append(counts[j])
     ylist = ylist[::-1]
-    print("ОСЬ Y: " + str(ylist))
+    #print("ОСЬ Y: " + str(ylist))
     costf = sum(xlist) + sum(ylist)
     return costf
 
 
 print(costfunction(sudoku))
+print(str(sudoku) + '\n')
 print(sudoku)
 
-for i in range(len(sudoku)):
-    for j in range(len(sudoku)):
-        if sudoku[i][j] == 0:
-            sudoku[i,j] = random.randint(1, 9)
-print(sudoku)
+
+def randomize():
+    array = np.copy(sudoku)
+    for i in range(len(array)):
+        for j in range(len(array)):
+            if array[i][j] == 0:
+                array[i, j] = random.randint(1, 9)
+    return array
+
+
+iterations = []
+s = []
+iteration = 1
+while True:
+    randsudoku = randomize()
+    costf = costfunction(randsudoku)
+    iterations.append(iteration)
+    s.append(costf)
+    if costf < 30 or len(s)>1000000:
+        #print(randsudoku)
+        #print(costf)
+        break
+    else:
+        #print(randsudoku)
+        #print(costf)
+        iteration += 1
+
+
+
+# График
+fig, ax = plt.subplots()
+ax.set(xlabel='Iteration (№)', ylabel='Costf (Sum of x and y)',
+       title='Sudoku')
+ax.plot(iterations, s)
+plt.grid(True)
+plt.show()
